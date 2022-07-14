121f441e14e699642849bccfbbcf95c5661e8966
Nelson Guerra <nelsonguerra.tech@outlook.com>
2022-07-13 20:14:52 -0400

    Created firebase config file to connect to firebase

---
+++ src/firebase.config.js
@@ -0,0 +1,24 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAHz21Plei6nxon5s79NGyerGZAFkCpYzU',
+  authDomain: 'andromeda-management.firebaseapp.com',
+  projectId: 'andromeda-management',
+  storageBucket: 'andromeda-management.appspot.com',
+  messagingSenderId: '325224357379',
+  appId: '1:325224357379:web:7283874d3437cf58ddb836',
+};
+
+// init firebase
+firebase.initializeApp(firebaseConfig);
+
+// init services
+const projectFirestore = firebase.firestore();
+const projectAuth = firebase.auth();
+
+// timestamp
+const timestamp = firebase.firestore.Timestamp;
+
+export { projectFirestore, projectAuth, timestamp };
