edb260612c6293d4a8d7efc9b0121310de50a520
Nelson Guerra <nelsonguerra.tech@outlook.com>
2022-07-15 19:42:21 -0400

    Project comments styling added

--- src/pages/project/ProjectComments.jsx
+++ src/pages/project/ProjectComments.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
 import { useState } from 'react';
 import { timestamp } from '../../firebase.config';
 import { useAuthContext } from '../../hooks/useAuthContext';
+import { useFirestore } from '../../hooks/useFirestore';
+import Avatar from '../../components/Avatar';
 
-export default function ProjectComments() {
-  const [newComment, setNewComment] = useState('');
+export default function ProjectComments({ project }) {
   const { user } = useAuthContext();
+  const { updateDocument, response } = useFirestore('projects');
+  const [newComment, setNewComment] = useState('');
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -18,25 +20,45 @@ export default function ProjectComments() {
       id: Math.random(),
     };
 
-    console.log(commentToAdd);
+    await updateDocument(project.id, {
+      comments: [...project.comments, commentToAdd],
+    });
+    if (!response.error) {
+      setNewComment('');
+    }
   };
 
   return (
     <div className='project-comments'>
       <h4>Project Comments</h4>
+
+      <ul>
+        {project.comments.length > 0 &&
+          project.comments.map((comment) => (
+            <li key={comment.id}>
+              <div className='comment-author'>
+                <Avatar src={comment.photoURL} />
+                <p>{comment.displayName}</p>
+              </div>
+              <div className='comment-date'>
+                <p>date here</p>
+              </div>
+              <div className='comment-content'>
+                <p>{comment.content}</p>
+              </div>
+            </li>
+          ))}
+      </ul>
+
       <form className='add-comment' onSubmit={handleSubmit}>
         <label>
           <span>Add new comment:</span>
           <textarea
-            required
+            onChange={(e) => setNewComment(e.target.value)}
             value={newComment}
-            onChange={(e) => {
-              setNewComment(e.target.value);
-            }}
           ></textarea>
         </label>
-
-        <button className='btn'> Add Comment</button>
+        <button className='btn'>Add Comment</button>
       </form>
     </div>
   );
