7c51c5d9a0354e568605737bce037779b0962534
Nelson Guerra <nelsonguerra.tech@outlook.com>
2022-07-15 18:01:09 -0400

    Started on dashboard styling and function, added ProjectList component

---
+++ src/components/ProjectList.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// styles
+import './ProjectList.css';
+
+export default function ProjectList({ projects }) {
+  return (
+    <div>
+      {projects.length === 0 && <p>No projects yet!</p>}
+      {projects.map((project) => (
+        <div key={project.id}>{project.name}</div>
+      ))}
+    </div>
+  );
+}
--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,20 +1,92 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import { useCollection } from '../../hooks/useCollection';
+import { timestamp } from '../../firebase.config';
+import { useAuthContext } from '../../hooks/useAuthContext';
+import { useFirestore } from '../../hooks/useFirestore';
+import Select from 'react-select';
 
 // styles
 import './Create.css';
 
+const categories = [
+  { value: 'development', label: 'Development' },
+  { value: 'design', label: 'Design' },
+  { value: 'sales', label: 'Sales' },
+  { value: 'marketing', label: 'Marketing' },
+];
+
 export default function Create() {
+  const history = useHistory();
+  const { addDocument, response } = useFirestore('projects');
+  const { documents } = useCollection('users');
+  const [users, setUsers] = useState([]);
+
   // form field values
   const [name, setName] = useState('');
   const [details, setDetails] = useState('');
   const [dueDate, setDueDate] = useState('');
   const [category, setCategory] = useState('');
   const [assignedUsers, setAssignedUsers] = useState([]);
+  const [formError, setFormError] = useState(null);
+  const { user } = useAuthContext();
+  // create user values for react-select
+  useEffect(() => {
+    if (documents) {
+      setUsers(
+        documents.map((user) => {
+          return { value: { ...user, id: user.id }, label: user.displayName };
+        })
+      );
+    }
+  }, [documents]);
+  // console.log(users)
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setFormError(null);
 
-    console.log(name, details, dueDate);
+    if (!category) {
+      setFormError('Please select a project category');
+      return;
+    }
+
+    if (assignedUsers.length < 1) {
+      setFormError('Please assign the project to at least one user');
+      return;
+    }
+
+    const createdBy = {
+      displayName: user.displayName,
+      photoURL: user.photoURL,
+      id: user.uid,
+    };
+
+    const assignedUsersList = assignedUsers.map((u) => {
+      return {
+        displayName: u.value.displayName,
+        photoURL: u.value.photoURL,
+        id: u.value.id,
+      };
+    });
+
+    // this saves to the firebase database
+    const project = {
+      name,
+      details,
+      category: category,
+      dueDate: timestamp.fromDate(new Date(dueDate)),
+      comments: [],
+      createdBy: createdBy,
+      assignedUsersList,
+    };
+
+    await addDocument(project);
+    if (!response.error) {
+      history.push('/');
+    }
+
+    console.log(project);
   };
 
   return (
@@ -49,14 +121,23 @@ export default function Create() {
         </label>
         <label>
           <span>Project category:</span>
-          {/* select here later */}
+          <Select
+            onChange={(option) => setCategory(option)}
+            options={categories}
+          />
         </label>
         <label>
           <span>Assign to:</span>
-          {/* select here later */}
+          <Select
+            onChange={(option) => setAssignedUsers(option)}
+            options={users}
+            isMulti
+          />
         </label>
 
         <button className='btn'>Add Project</button>
+
+        {formError && <p className='error'>{formError}</p>}
       </form>
     </div>
   );
